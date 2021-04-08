4fc170bce776c48ebe373ed17afd4efb42f8fa7f
neo hong <neo.hong@tron.network>
2021-04-08 14:37:32 +0800

    adapt to tron

--- libsolidity/parsing/Parser.cpp
+++ libsolidity/parsing/Parser.cpp
@@ -1781,12 +1781,15 @@ ASTPointer<Expression> Parser::parsePrimaryExpression()
 	case Token::Number:
 		if (TokenTraits::isEtherSubdenomination(m_scanner->peekNextToken()))
 		{
-			ASTPointer<ASTString> literal = getLiteralAndAdvance();
-			nodeFactory.markEndPosition();
-			Literal::SubDenomination subdenomination = static_cast<Literal::SubDenomination>(m_scanner->currentToken());
-			m_scanner->next();
-			expression = nodeFactory.createNode<Literal>(token, literal, subdenomination);
-		}
+            fatalParserError(string("Ether unit denomination is not supported by the compiler"));
+        }
+        else if (TokenTraits::isTronSubdenomination(m_scanner->peekNextToken())) {
+            ASTPointer<ASTString> literal = getLiteralAndAdvance();
+            nodeFactory.markEndPosition();
+            Literal::SubDenomination subdenomination = static_cast<Literal::SubDenomination>(m_scanner->currentToken());
+            m_scanner->next();
+            expression = nodeFactory.createNode<Literal>(token, literal, subdenomination);
+        }
 		else if (TokenTraits::isTimeSubdenomination(m_scanner->peekNextToken()))
 		{
 			ASTPointer<ASTString> literal = getLiteralAndAdvance();
